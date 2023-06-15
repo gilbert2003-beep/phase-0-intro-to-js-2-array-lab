56aaafcdeb940b67194b27ad5218db65e2ba956b
gilbert <gilbert.bageni@student.moringaschool.com>
2023-06-15 20:23:34 +0300

    done

--- index.js
+++ index.js
@@ -1,1 +1,17 @@
 // Write your solution here!
+let cats = ["Milo", "Otis", "Garfield"];
+
+function destructivelyAppendCat(name) {
+    cats.push(name);
+    }
+    function destructivelyPrependCat(name) {
+        cats.unshift(name);
+        }
+        function destructivelyRemoveLastCat(name) {
+            let lastCat = cats.pop();
+            return lastCat;
+
+        }
+        destructivelyRemoveFirstCat(); {
+        let firstCat = cats.shift();
+    };
